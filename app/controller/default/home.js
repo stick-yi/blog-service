56689f639b4fc750839aee2a4cf1a18b89063224
StickYi <stick_yi@163.com>
2019-11-06 18:04:15 +0800

    add 列表页接口，和文章类型接口

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -8,6 +8,7 @@ class HomeController extends Controller {
     ctx.body = "api hi";
   }
 
+  // 列表查询
   async getAtrticleList() {
     let sql = 'SELECT article.id as id ,' + 
               'article.title as title ,' +
@@ -21,6 +22,7 @@ class HomeController extends Controller {
     this.ctx.body = { data: results }
   }
 
+  // 通过id查询文章详情
   async getArticleById() {
     let id = this.ctx.params.id
 
@@ -39,6 +41,27 @@ class HomeController extends Controller {
     this.ctx.body = {data: result}
   }
 
+  // 得到类别名称和编号
+  async getTypeInfo() {
+    const result = await this.app.mysql.select('type')
+    this.ctx.body = {data: result}
+  }
+
+  // 根据类别id获得文章列表
+  async getListById() {
+    let id = this.ctx.params.id
+    let sql = 'SELECT article.id as id ,' + 
+              'article.title as title ,' +
+              'article.introduce as introduce ,' +
+              "FROM_UNIXTIME(article.addTime, '%Y-%m-%d %H:%i:%s') as addTime ," +
+              'article.viewCount as viewCount ,' +
+              'type.typeName as typeName ' +
+              'FROM article LEFT JOIN type ON article.typeId = type.id ' + 
+              'WHERE typeId='+id
+
+    const results = await this.app.mysql.query(sql)
+    this.ctx.body = { data: results }
+  }
 
 
 }
